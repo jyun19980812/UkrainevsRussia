90a61b8ef610631d547a688e76013a3dfea55409
Jangwon Yun <jangwonyun@Jangwonui-MacBookPro.local>
2022-05-23 00:03:54 -0700

    file changing

--- main.py
+++ main.py
@@ -4,14 +4,10 @@ Jangwon Yun, Victor Ahn, Yongjung Lee
 Why Russia Should Stop War
 """
 import pandas as pd
-import seaborn as sns
-import matplotlib.pyplot as plt
-from sklearn.model_selection import train_test_split
-from sklearn.tree import DecisionTreeRegressor
-from sklearn.metrics import mean_squared_error
 
-# Press the green button in the gutter to run the script.
+def main():
+    eq = pd.read_csv('/Users/jangwonyun/Desktop/UkrainevsRussia/russia_losses_equipment.csv')
+    print(eq)
+
 if __name__ == '__main__':
-
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    main()
